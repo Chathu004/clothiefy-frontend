b34a2d9c467bac888db15344e7113abf6f85238f
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-04-16 13:49:32 +0530

    set router paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
 import './App.css'
 import Navigation from './customer/components/Navigation'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { Shop } from '@mui/icons-material';
+import ShopCategory from './pages/ShopCategory';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import LoginSignUp from './pages/LoginSignUp';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <div className="">
+      <BrowserRouter>
       <Navigation/>
+      
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/men' element={<ShopCategory category="men"/>}/>
+        <Route path='/women' element={<ShopCategory category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kids"/>}/>
+          <Route path='/product' element={<Product />}/>
+        <Route/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignUp/>}/>
+      </Routes>
+      
+      </BrowserRouter>
      
      
     </div>
